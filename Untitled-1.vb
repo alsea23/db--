cfd66f72bcc2df4fce70d630f920c94145026267
이창현 <dutneoqkd@naver.com>
2026-03-04 10:26:38 +0900

    commit

--- Untitled-1.vb
+++
@@ -1,7 +0,0 @@
-
-					GRID_PROJECT_DETAIL.GetRow(i).GetCell("SIMUL_WORK_CNTR_NM").BackColor = "#e5e7e9";				
-					GRID_PROJECT_DETAIL.GetRow(i).GetCell("DEFAULT_YN").BackColor = "#5678e8";
-					GRID_PROJECT_DETAIL.GetRow(i).GetCell("CHK").BackColor = "#fff2cc";
-					GRID_PROJECT_DETAIL.GetRow(i).GetCell("WORK_DAYS").BackColor = "#fff2cc";
-					GRID_PROJECT_DETAIL.GetRow(i).GetCell("PREPARE_DAYS").BackColor = "#fff2cc";
-					GRID_PROJECT_DETAIL.GetRow(i).GetCell("PRD_STRT_DATE").BackColor = "#fff2cc";
